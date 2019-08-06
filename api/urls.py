7a8eeee3c56e6f395cfb938458256ebc59382aca
Praneeth Karnena <praneeth.codes@gmail.com>
2019-08-07 00:00:05 +0530

    configured home page

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home_view),
+    path('api/', views.home_view),
+    path('api/v1/', views.home_view)
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_GET
 
 
+@require_GET
 def home_view(request):
-    pass
+    return render(request, 'api/home.html')
