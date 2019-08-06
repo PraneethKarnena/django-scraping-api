2bbae0da100dd26b96b61dce81060042d70e68f2
Praneeth Karnena <praneeth.codes@gmail.com>
2019-08-07 00:06:31 +0530

    configuring API

--- api/views.py
+++ api/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.views.decorators.http import require_GET
+from rest_framework.decorators import (api_view, permission_classes)
+from rest_framework.permissions import AllowAny
 
 
 @require_GET
 def home_view(request):
     return render(request, 'api/home.html')
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def scrape_view(request):
+    pass
